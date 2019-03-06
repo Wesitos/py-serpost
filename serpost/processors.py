e867e701c86b9672e2720d85f57328cfd034cf62
Pedro Palacios Avila <wesitos@makerlabperu.org>
2019-03-06 11:18:15 -0500

    Fix date parse bug

--- serpost/processors.py
+++ serpost/processors.py
@@ -1,6 +1,21 @@
 """Json response handling logic"""
+import logging
 from datetime import datetime
 from bs4 import BeautifulSoup
+
+logger = logging.getLogger(__name__)
+
+def process_fecha(raw_fecha):
+    'Parses the date string from a package details row'
+    if not raw_fecha:
+        return None
+
+    try:
+        return datetime.strptime(raw_fecha, '%m/%d/%Y %I:%M:%S %p')
+    except ValueError:
+        logger.exception('Obtenida una cadena de fecha infalida {}'
+                         .format(repr(raw_fecha)))
+        return None
 
 
 def process_package_details(details):
@@ -13,13 +28,13 @@ def process_package_details(details):
 
     return [dict(
         destino=destino,
-        fecha=datetime.strptime(fecha, '%d/%m/%Y'),
+        fecha=process_fecha(fecha),
         descripcion=texto,
     ) for (destino, fecha, texto) in row_list]
 
 
 def process_package_summary(summary):
-    """Clenas the response of the summary request"""
+    """Cleans the response of the summary request"""
     field_dict = dict(
         año=('RetornoCadena1', int),
         codigo=('RetornoCadena2', str),
